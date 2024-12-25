673b35a37e8feb98765183c2206d261fc0f6e818
Rom <vanson010825@gmail.com>
2024-12-25 21:35:14 +0700

    initial guess the number

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  //   document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.score').style.width = '15rem';
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    // document.querySelector('.message').textContent = '😒 No number!';
+    displayMessage('😒 No number!');
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = '🎊 Correct Number';
+    displayMessage('🎊 Correct Number');
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess !== secretNumber) {
+    // document.querySelector('.message').textContent = guess > secretNumber ? '🤡 Too High' : '💀 Too Low';
+    displayMessage(guess > secretNumber ? '🤡 Too High' : '💀 Too Low');
+    score--;
+    document.querySelector('.score').textContent = score;
+    if (score < 1) {
+      //   document.querySelector('.message').textContent = '😒 You Lost The Game! Nonnnnnnnnnnnnnnnnn';
+      displayMessage('😒 You Lost The Game! Nonnnnnnnnnnnnnnnnn');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
